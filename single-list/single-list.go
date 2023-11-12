6ae4a21b491fe421a2179a51466602832215423d
CukorZhong <2450808861@qq.com>
2023-11-12 13:58:33 +0800

    添加了头插入功能，并测试通过

--- single-list/single-list.go
+++ single-list/single-list.go
@@ -1,38 +1,56 @@
 package singlelist
 
+import "fmt"
+
 // 单链表
 type SingleList[T comparable] struct {
-	Head *node[T]
-	Rear *node[T]
+	head *node[T]
+	rear *node[T]
 	size uint
 }
 
 // 单链表构造函数
 func NewSingleList[T comparable]() *SingleList[T] {
 	sl := new(SingleList[T])
-	sl.Head = nil
-	sl.Rear = nil
+	sl.head = nil
+	sl.rear = nil
 	sl.size = 0
 	return sl
 }
 
 // 获取链表的大小
-func (sl SingleList[T]) Size() uint {
+func (sl *SingleList[T]) Size() uint {
 	return sl.size
 }
 
 // 判断链表是否为空链表
-func (sl SingleList[T]) Empty() bool {
+func (sl *SingleList[T]) Empty() bool {
 	return sl.size == 0
 }
 
-// 头插入
-func (sl SingleList[T]) Preappend(data T) *node[T] {
+// 遍历单链表
+func (sl *SingleList[T]) Show() {
+	for p := sl.head; p != nil; p = p.Next {
+		fmt.Printf("%v ", p.Data)
+	}
+	fmt.Println()
+}
 
-	return nil
+// 头插入
+func (sl *SingleList[T]) Preappend(data T) *node[T] {
+	n := NewNode[T](data, nil)
+	if sl.size == 0 {
+		sl.head = n
+		sl.rear = n
+	} else {
+		n.Next = sl.head
+		sl.head = n
+	}
+	sl.size++
+	return n
 }
 
 // 尾插入
-func (sl SingleList[T]) Append(data T) *node[T] {
+func (sl *SingleList[T]) Append(data T) *node[T] {
 	return nil
 }
--- single-list/single-list_test.go
+++ single-list/single-list_test.go
@@ -1,7 +1,15 @@
 package singlelist
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestSingleList(t *testing.T) {
-
+	sl := NewSingleList[int]()
+	sl.Preappend(66)
+	sl.Preappend(33)
+	sl.Preappend(99)
+	fmt.Printf("size = %d\n", sl.Size())
+	sl.Show()
 }
